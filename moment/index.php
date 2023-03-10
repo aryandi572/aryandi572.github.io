f11dadb2b97ea95c18a6848caca3c90f86475fef
boy <aryandithree@gmail.com>
2023-03-10 10:35:49 +0700

    Add files via upload

---
+++ moment/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Love You</title>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<!-- <script>window.jQuery || document.write('')</script> -->
+<script src="asset/js/jquery-1.10.2.min.js"></script>
+<script src="asset/js/jquery.flexslider.js"></script>
+<script src="asset/js/init.js"></script>
+
+<body>
+
+    <div style="padding-bottom: 177.78%; position: relative;" hidden><iframe
+            style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;"
+            src="https://www.youtube.com/embed/iZpZDivj6SU?autoplay=1&controls=0&loop=1&modestbranding=1&playlist=iZpZDivj6SU&rel=0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" width="100%"
+            height="100%" frameborder="0"><small>Powered by <a
+                    href="https://embed.tube/embed-code-generator/youtube/">youtube embed video</a>
+                generator</small></iframe></div>
+
+    <!-- <iframe src="https://dalmop.disdukcapilsurabaya.id/html/ChristinaPerriAThousandYears.mp3" type="audio/mp3" allow="autoplay" id="audio"
+		style="display:none"></iframe> -->
+
+    <!-- <iframe width="420" height="345" src="https://www.youtube.com/embed/iZpZDivj6SU?autoplay=1&mute=1" frameborder="0"
+			allowfullscreen ></iframe> -->
+
+    <!-- <iframe src="http://www.youtube.com/embed/iZpZDivj6SU?autoplay=1" allow="autoplay"></iframe> -->
+    <!-- <iframe width="560" height="315" src="https://dalmop.disdukcapilsurabaya.id/html/ChristinaPerriAThousandYears.mp3" frameborder="0" allowfullscreen></iframe> -->
+    <div class="wrapper">
+        <div class="container">
+            <div class="main">
+                <section id="testimonials">
+                    <div class="text-container">
+                        <div class="row">
+                            <div class="two columns header-col">
+                                <h1><span>Quotes</span></h1>
+                            </div>
+                            <div class="ten columns flex-container">
+                                <div class="flexslider">
+                                    <ul class="slides">
+                                        <li>
+                                            <blockquote>
+                                                <p>
+                                                    “Kita ini kumpulan masa lalu, yang untuk memulai langkah baru,
+                                                    hanya butuh satu restu.”
+                                                </p>
+                                                <cite></cite>
+                                            </blockquote>
+                                        </li>
+                                        <li>
+                                            <blockquote>
+                                                <p>
+                                                    “Buatku, satu-satunya yang bisa kulakukan kalau takut kehilangan
+                                                    seseorang adalah dengan berusaha
+                                                    menjadi pantas untuk
+                                                    tidak ditinggalkan.”
+                                                </p>
+                                                <cite>I Love You ♥</cite>
+                                            </blockquote>
+                                        </li>
+                                        <li>
+                                            <blockquote>
+                                                <p>
+                                                    “Kadang yang membuat kita sedih bukan seperti apa rindu itu datang,
+                                                    tapi masih adakah yang kita rindukan
+                                                    itu.”
+                                                </p>
+                                                <cite>Makasih iya sayang ♥</cite>
+                                            </blockquote>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <ul class="bg-bubbles">
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+                <li>
+                    <div class="heart pulse2">♥</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- <audio controls autoplay style="display:block;">
+	<source src="asset/audio/ChristinaPerri-AThousandYears.mp3" type="audio/mpeg">
+	Your browser does not support the audio element.
+</audio> -->
+
+</body>
+
+</html>
